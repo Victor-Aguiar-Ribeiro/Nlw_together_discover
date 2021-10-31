ccb5da425162beaa313409d9bc273293b8987c12
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-10-31 18:18:37 -0300

    importing project to Node
    Created the main directories structure of the node project
    Created routes.js file
    moved *.html files to ./src/views and changed them to .ejs
    Added nodemon as dev dependency `yarn add nodemon -D`
    
    Created public directory for the assets and styles
    Changed assets to images (also in the *.ejs and home.css files)
    
    Added a route to the index page, already working fine

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+﻿const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => {res.render('index')})
+
+module.exports = route
--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
-﻿const express = require ('express')
+﻿const express = require ( 'express' )
+const route = require ( './routes' )
+const path = require ( 'path' )
 
 const server = express()
+
+server.set( 'view engine', 'ejs' )
+
+server.set( 'views', path.join(__dirname, 'views'))
+
+server.use( route )
+
+server.use( express.static( 'public' ) )
 
 server.listen ( 3000, () => console.log('🚀 Running on: http://localhost:3000') )
