a3056ead7681c77257c2d587df41fdf81de078b4
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-01 06:38:59 -0300

    Added modal functionallities

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,38 @@
-﻿import { Modal } from "./modal"
+﻿import Modal from "./modal.js"
 
 const modal = Modal()
 
-// Pegar todos os botões com a classe 'check'
-const checkButtons = document.querySelectorAll( '.actions .check' )
+const modalTitle = document.querySelector('.modal .title')
+const modalParagraph = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal .modal-form .buttons .--btn-red')
 
-// Adicionar um event listener em cada botão check para
-checkButtons.forEach( ( button ) => {
+// Pegar todos os botões com a classe 'check'
+const checkButtons = document.querySelectorAll( '.actions a.check' )
+
+// Adicionar um event listener em cada botão check
+checkButtons.forEach( button => {
   button.addEventListener( 'click', event => { 
+    modalTitle.innerHTML = 'Marcar como lida'
+    modalParagraph.innerHTML = 'Tem certeza que você deseja marcar esta pergunta como lida?'
+    modalButton.innerHTML = 'Sim, tenho'
+
     // Abrir modal
-    modal.open( event )
+    modal.open( event );
+  })
+})
+
+// Pegar todos os botões com a classe 'trash'
+const trashButtons = document.querySelectorAll( '.actions a.trash' )
+
+// Adicionar um event listener em cada botão trash 
+trashButtons.forEach( button => {
+  button.addEventListener( 'click', event => { 
+    modalTitle.innerHTML = 'Excluir pergunta'
+    modalParagraph.innerHTML = 'Tem certeza que você deseja excluir esta pergunta?'
+    modalButton.innerHTML = 'Sim, excluir'
+
+    // Abrir modal
+    modal.open( event );
   })
 })
 
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,14 +1,19 @@
-﻿module.exports = function Modal(){
+﻿export default function Modal(){
+
+  const modalWrapper = document.querySelector('#modalWrapper')
+
+  // Pegar botão de cancelar da Modal e chamar a função close -> onclick
+  const cancelButton = document.querySelector('.buttons .cancel')
+  cancelButton.addEventListener('click', close)
+
   function open() {
     // Atribui a classe 'active' à modalWrapper
-
-
+    modalWrapper.classList.add('active')
   }
 
   function close() {
     // Remove a classe 'active' da modalWrapper
-
-
+    modalWrapper.classList.remove('active')
   }
 
   return{ 
