4f65096ff36d7d6c8eed59c1151279a5d1bfc84b
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-01 20:40:12 -0300

    Created the application Database with SQLite driver
    Installed SQLite dependencies `yarn add sqlite`
    Created ./src/db directory
    Created init.js an config.js files inside /.db directory
    Implemented the tables that are going to be used into the DB
    Database file included -> ocketq.sqlite

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,13 @@
 ﻿module.exports = {
   createRoom( req, res ){
-    let roomId = 12345
+    let roomId = ''
 
-    res.redirect( `/room/${ roomId }` )
+    for( let i = 0; i < 6 ; i++ ){
+      roomId += `${ Math.floor( Math.random() * 10 ) }`
+    }
+
+    console.log(roomId)
+
+    res.redirect( `/room/${ parseInt(roomId) }` )
   }
 }
