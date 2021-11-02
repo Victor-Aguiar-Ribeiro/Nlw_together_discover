7f30ccaba12618faf31e3ef0b3cecad885f918f6
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-02 11:30:26 -0300

    Madking sure there will only exist one roomId of a kind on DB

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,21 +5,32 @@ module.exports = {
     const db = await Database()
     const password = req.body.password
     
-    // Criar código da sala de forma randômica 
     let roomId = ''
+    let isRoom = true
+    
+    // Garantir que o roomId gerado seja unico no DB -> while ( existir um id igual no DB ) => gerar novo id
+    while( isRoom ){
+      // Criar código da sala de forma randômica 
+      for( let i = 0; i < 6 ; i++ ){
+        roomId += `${ Math.floor( Math.random() * 10 ) }`
+      }
 
-    for( let i = 0; i < 6 ; i++ ){
-      roomId += `${ Math.floor( Math.random() * 10 ) }`
+      // Verificar se o roomId já existe no DB
+      const existId = await db.all( `SELECT id FROM rooms` )
+      isRoom = existId.some( exist => exist === existId )
+
+      if( !isRoom ){
+        // Aqui são passados o password digitado ao criar a sala, e a room ID gerada randomicamente para a tabela `rooms` criada no DB
+        await db.run( `INSERT INTO rooms (
+          id,
+          password
+        ) VALUES (
+          ${ parseInt( roomId ) },
+          ${ password }
+        )`)
+        
+      }
     }
-
-    // Aqui são passados o password digitado ao criar a sala, e a room ID gerada randomicamente para a tabela criada no DB
-    await db.run( `INSERT INTO rooms (
-      id,
-      password
-    ) VALUES (
-      ${ parseInt( roomId ) },
-      ${ password }
-    )`)
 
     await db.close()
 
