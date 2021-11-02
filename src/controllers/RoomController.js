8da8befb5af420c1618fd84b2b4caf9a8e44c3ec
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-01 22:45:18 -0300

    Added create-room functionality
    Now, when a room is created, the written password is automatically sent to the DB, together with a random created Room ID

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,12 +1,27 @@
-﻿module.exports = {
-  createRoom( req, res ){
+﻿const Database = require('../db/config')
+
+module.exports = {
+  async createRoom( req, res ){
+    const db = await Database()
+    const password = req.body.password
+    
+    // Criar código da sala de forma randômica 
     let roomId = ''
 
     for( let i = 0; i < 6 ; i++ ){
       roomId += `${ Math.floor( Math.random() * 10 ) }`
     }
 
-    console.log(roomId)
+    // Aqui são passados o password digitado ao criar a sala, e a room ID gerada randomicamente para a tabela criada no DB
+    await db.run( `INSERT INTO rooms (
+      id,
+      password
+    ) VALUES (
+      ${ parseInt( roomId ) },
+      ${ password }
+    )`)
+
+    await db.close()
 
     res.redirect( `/room/${ parseInt(roomId) }` )
   }
