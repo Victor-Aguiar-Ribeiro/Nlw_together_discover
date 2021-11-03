cf316250c49c4df42109e55d99c79ff586148b1b
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-02 21:06:58 -0300

    Check and Delete questions functionality integrated with DB

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,14 +1,29 @@
 ﻿const Database = require('../db/config')
 
 module.exports = {
-  index( req, res ) {
+  async index( req, res ) {
+    const db = await Database()
+
     const roomId = req.params.room
     const questionId = req.params.question
     const action = req.params.action
-
     const password = req.body.password
 
-    console.log(`Room id: ${roomId} ; Question id: ${questionId} ; Action: ${action} ; Password: ${password}`)
+    // console.log(`Room id: ${roomId} ; Question id: ${questionId} ; Action: ${action} ; Password: ${password}`)
+
+    // Verificação da senha
+    const verifyRoom = await db.get( `SELECT * FROM rooms WHERE id = ${roomId}` )
+
+    if( verifyRoom.password === password ) {
+      if( action === 'trash' ) {
+        await db.run( `DELETE FROM questions WHERE id = ${questionId}` )
+      }else if( action === 'check' ) {
+        await db.run( `UPDATE questions SET isChecked = 1 WHERE id = ${questionId}` )
+      }
+    }
+    res.redirect( `/room/${roomId}` )
+
+    await db.close()
   },
 
   async createQuestion( req, res ) {
